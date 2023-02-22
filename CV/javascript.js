cf9b859c5a54ac29bd863c91e74ae4828acf444b
esteban rivero <estebanmr9@hotmail.com>
2023-02-22 14:33:43 -0300

    primer escritura en el repositorio

---
+++ CV/javascript.js
@@ -0,0 +1,50 @@
+/**Java Scrip para pagina de CV */
+
+
+/**Cambiar modo de lectura*/
+
+function cambiarmodo() {
+    var element = document.body;
+    element.classList.toggle("Modo-luz");}
+
+
+/**Boton para volver arriba*/
+
+let mybutton = document.getElementById("subir");
+    
+function subir() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;}
+
+
+/**calculadora de edad*/
+
+function sumarfechas(){
+    let numeroA = document.getElementById("numero1").value;
+    let numeroB = -1991;
+    
+    let ResultadoPrueba = suma(parseInt(numeroA), parseInt(numeroB));
+    
+    
+       document.getElementById("resultado").innerHTML = ResultadoPrueba;
+    };
+
+function suma(a,b){return a+b}    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     
+
+
